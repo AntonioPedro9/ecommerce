7cba3699a074f22edc9f084e8fef3551e2ed3ce9
Antônio Pedro <antoniopedro.rs9@gmail.com>
2025-08-06 00:55:12 -0300

    feat: add ui for sign in and sign up form components

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,27 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import SignInForm from "./components/sign-in-form";
+import SignUpForm from "./components/sign-up-form";
+
+const Authentication = () => {
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
+      <Tabs defaultValue="signin">
+        <TabsList>
+          <TabsTrigger value="signin">Entrar</TabsTrigger>
+          <TabsTrigger value="signup">Criar conta</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="signin">
+          <SignInForm />
+        </TabsContent>
+
+        <TabsContent value="signup">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Authentication;
