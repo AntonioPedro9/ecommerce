901f15662206bf8256d1bdacf22f594af72a6227
Antônio Pedro <antoniopedro.rs9@gmail.com>
2025-08-07 11:08:50 -0300

    feat: add newly added products list and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  const newlyAddedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
     with: {
       variants: true,
     },
@@ -46,6 +56,10 @@ const Home = async () => {
             className="w-full h-auto"
           />
         </div>
+
+        <ProductList title="Novos produtos" products={newlyAddedProducts} />
+
+        <Footer />
       </div>
     </>
   );
